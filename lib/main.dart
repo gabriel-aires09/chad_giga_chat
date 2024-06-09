2f3fd0a7eac7776bde01b09197f8fb42635dfdc9
Gabriel Oliveira Aires <90778217+gabriel-aires09@users.noreply.github.com>
2024-06-09 00:02:13 -0300

    removendo linhas desnecessarias main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:chad_giga_chat/themes/light_mode.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-// 50:01 https://www.youtube.com/watch?v=5xU5WH2kEc0
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
